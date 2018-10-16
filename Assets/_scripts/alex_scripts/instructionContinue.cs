df0873fa48de1b7ae0b5eb49662c0dc9c92a7bed
Alex <32230405+r3doctober@users.noreply.github.com>
2018-10-16 00:07:12 -0600

    Main menu && Instruction menu

---
+++ Assets/_scripts/alex_scripts/instructionContinue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class instructionContinue : MonoBehaviour {
+    public Button continueButton;
+    public GameObject continueButtonObject;
+    
+    float startDelay = 5f;
+    float startTimer;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Button loadButton = continueButton.GetComponent<Button>();
+        continueButtonObject.SetActive(false);
+        loadButton.onClick.AddListener(LoadLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startTimer += Time.deltaTime;
+        if (startDelay <= startTimer)
+        {
+            continueButtonObject.SetActive(true);
+        }
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Assets/_scripts/alex_scripts/loadButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class loadButton : MonoBehaviour {
+    public Button startButton;
+    public GameObject startButtonObject;
+
+    public Button quitButton;
+    public GameObject quitButtonObject;
+
+
+	// Use this for initialization
+	void Start () {
+
+        Button loadButton = startButton.GetComponent<Button>();
+        Button exitButton = quitButton.GetComponent<Button>();
+
+        startButtonObject.SetActive(false);
+        quitButtonObject.SetActive(false);
+
+        loadButton.onClick.AddListener(LoadLevel);
+        exitButton.onClick.AddListener(Quit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+    
+}
